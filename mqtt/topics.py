24c0bb588dd5e1cd51ab3e4c2abd5fbb26eb5269
Hajiwo <jundong202917@gamil.com>
2026-07-01 14:07:57 +0200

    feat(mqtt topics): implement MQTT sensor topics

--- mqtt/topics.py
+++ mqtt/topics.py
@@ -1,6 +1,17 @@
-"""MQTT topic definitions."""
+"""
+MQTT topics used by the backend
+"""
 
-SENSOR_TEMPERATURE = "garage/sensor/temperature"
-SENSOR_VEHICLE = "garage/sensor/vehicle"
-COMMAND_DOOR = "garage/command/door"
-COMMAND_LIGHT = "garage/command/light"
+from dataclasses import dataclass
+
+class SensorTopics:
+    """MQTT topics for garage sensors."""
+    SENSOR_TEMPERATURE = "garage/sensor/temperature"
+    SENSOR_LIGHT = "garage/sensor/light"
+    PARKING = "garage/sensor/parking"
+    LICENSE = "garage/sensor/license"
+
+sensorTopic = SensorTopics()
+
+class ActuatorTopics:
+    """MQTT topics for actuators"""
