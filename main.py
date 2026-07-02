314267038b7f4afb79767ef28db3c08687d16ce0
Hajiwo <jundong202917@gamil.com>
2026-07-02 16:23:33 +0200

    add(main.py): add instance of context manager

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 """Entry point for the Smart Garage application."""
 from mqtt.client import MQTTClient
 from mqtt.eventDispatcher import MQTTEventDispatcher
-from mqtt.topics import SensorTopics, CommandTopics
+from mqtt.topics import SensorTopics
+from context.manager import ContextManager
 
 def main() -> None:
     """Run the application."""
@@ -10,13 +11,15 @@ def main() -> None:
     mqtt_client = MQTTClient()
     event_dispatcher = MQTTEventDispatcher()
     sensor_topics = SensorTopics()
+    context_manager = ContextManager()
     sensor_topics_list = sensor_topics.get_topics()
+
     for sensor_topic in sensor_topics_list:
         mqtt_client.subscribe(sensor_topic, event_dispatcher.push_event)
+  
     while True:
         event = event_dispatcher.get_event()
-        #Process(event)
-
+        context_manager.event_handler(event)
 
 
 if __name__ == "__main__":
